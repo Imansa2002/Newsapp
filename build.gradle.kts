fbcd7616cc0d2dab6ddf27e2d6d88a1c6f8c5788
Imansa2002 <167952948+Imansa2002@users.noreply.github.com>
2025-06-11 21:32:42 +0530

    Update news screen

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 // Top-level build.gradle.kts
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.android.tools.build:gradle:8.8.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     alias(libs.plugins.google.gms.google.services) apply false
 }
